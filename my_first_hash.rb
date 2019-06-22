249dc35f0cc39bb0ec59005d27cc9366f06807e3
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 01:43:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,7 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs 
+  first_hash = {"my_name" => "Rashad Walcott"
+  
 
   
 end
